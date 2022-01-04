7cf9029d85a38392f0b3616bffa65172a04a8457
Jobayer-Majumder <jobayermojumder70@gmail.com>
2022-01-05 00:11:32 +0600

    added bcrypt for hashing password

--- graphql/resolvers/loginResolver.js
+++ graphql/resolvers/loginResolver.js
@@ -1,19 +1,20 @@
-const loginModel = require("../../models/loginModel")
+const bcrypt = require('bcrypt');
+const loginModel = require("../../models/loginModel");
+
 
 
 module.exports = {
     users: async () => {
         try {
-            const users = await loginModel.find()
-
+            const users = await loginModel.find({});
             return users
-
         } catch (error) {
-            throw new Error('Authentication failed! Please try again')
+            throw new Error('Something went wrong! Please try again')
         }
     },
     addUser: async args => {
-        const {name, email, password, role} = args.input;
+        const { name, email, password, role } = args.input;
+
         const user = new loginModel({
             name: name,
             email: email,
@@ -21,24 +22,31 @@ module.exports = {
             role: role
         })
         try {
-            const users = await user.save();
-            return users
+            const isExisting = await loginModel.find({ email: email });
+            if (!isExisting[0]) {
+                const users = await user.save();
+                return users
+            } else {
+                throw new Error('This email already registered!')
+            }
         } catch (error) {
             throw new Error('Something went wrong with creating user!');
-        }
+        };
     },
     findUser: async args => {
-        const {email, password} = args.input;
+        const { email, password } = args.input;
 
         try {
-            const user = await loginModel.findOne({email: email, password: password});
-            if(user){
+            const user = await loginModel.findOne({ email: email });
+            const validPassword = await bcrypt.compare(password, user.password);
+            
+            if (user && validPassword) {
                 return user
-            }else{
+            } else {
                 throw new Error('Credential not valid!')
             }
         } catch (error) {
             throw new Error('Something went wrong with finding user!')
         }
     }
-}
+};
--- models/loginModel.js
+++ models/loginModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 
 
 const LoginModel = new mongoose.Schema({
@@ -21,4 +21,15 @@ const LoginModel = new mongoose.Schema({
     }
 }, {timestamps: true})
 
-module.exports = mongoose.model('users', LoginModel)
+
+LoginModel.pre('save', async function(next){
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hashedPassword;
+    } catch (error) {
+        next(error)
+    }
+});
+
+module.exports = mongoose.model('users', LoginModel);
